f6109668d35a666d394f3b5247318cb6c669d3ff
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-07-25 15:52:02 -0600

    dashboard exercise

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,93 @@
 import React, { Component } from "react";
-
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+import Loading from "./Loading";
+import Panel from "./Panel";
 
-    return <main className={dashboardClasses} />;
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+class Dashboard extends Component {
+  state = {
+    loading: false,
+    focused: null
+  };
+
+  //Class property with arrow function
+  // selectPanel = id => {
+  //   this.setState({ 
+  //     focused: id 
+  //   });
+  // };
+  
+  //instance method
+  // selectPanel(id) {
+  //   this.setState({ 
+  //     focused: null
+  //   });
+  // }
+
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", { "dashboard--focused": this.state.focused });
+    //const { id, label, value, onSelect } = this.props; 
+
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panelArr = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map((panel) => {
+      return (
+
+        // <section className="dashboard__panel" onClick={(event) => onSelect(id)}>
+          <Panel
+            key={panel.id}
+            // id={panel.id}
+            label={panel.label}
+            value={panel.value}
+            onSelect={() => this.selectPanel(panel.id)}
+            //onClick={() => onSelect(id)}
+          
+          />
+
+        //</section>
+      );
+    });
+
+
+    return (
+      <main className={dashboardClasses}> 
+        {panelArr}
+      </main>
+    );
+
   }
 }
 
