54adb8dc98ba5b9df8476f32397b8ebc2657b9e4
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-18 10:08:02 -0600

    Dashboard setup and the one 'Loading' panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,93 +1,21 @@
-import React, { Component } from "react";
-import classnames from "classnames";
-
-import Loading from "./Loading";
-import Panel from "./Panel";
-
-const data = [
-  {
-    id: 1,
-    label: "Total Interviews",
-    value: 6
-  },
-  {
-    id: 2,
-    label: "Least Popular Time Slot",
-    value: "1pm"
-  },
-  {
-    id: 3,
-    label: "Most Popular Day",
-    value: "Wednesday"
-  },
-  {
-    id: 4,
-    label: "Interviews Per Day",
-    value: "2.3"
-  }
-];
+import React, {Component} from 'react';
+import classnames from 'classnames';
+import Loading from './Loading';
 
 class Dashboard extends Component {
-  state = {
-    loading: false,
-    focused: null
+  state = { 
+    loading: false 
   };
 
-  //Class property with arrow function
-  // selectPanel = id => {
-  //   this.setState({ 
-  //     focused: id 
-  //   });
-  // };
-  
-  //instance method
-  // selectPanel(id) {
-  //   this.setState({ 
-  //     focused: null
-  //   });
-  // }
-
-  selectPanel(id) {
-    this.setState(previousState => ({
-      focused: previousState.focused !== null ? null : id
-    }));
-  }
 
   render() {
-    const dashboardClasses = classnames("dashboard", { "dashboard--focused": this.state.focused });
-    //const { id, label, value, onSelect } = this.props; 
-
+    const dashboardClasses = classnames("dashboard");
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panelArr = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
-    .map((panel) => {
-      return (
-
-        // <section className="dashboard__panel" onClick={(event) => onSelect(id)}>
-          <Panel
-            key={panel.id}
-            // id={panel.id}
-            label={panel.label}
-            value={panel.value}
-            onSelect={() => this.selectPanel(panel.id)}
-            //onClick={() => onSelect(id)}
-          
-          />
-
-        //</section>
-      );
-    });
-
-
-    return (
-      <main className={dashboardClasses}> 
-        {panelArr}
-      </main>
-    );
-
+    return <main className={dashboardClasses} />;
   }
 }
 
